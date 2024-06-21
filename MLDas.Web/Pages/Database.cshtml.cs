1c14813559e388552004ebb55c0720b26e5e3b74
Jarred Capellman <jcapellman@hotmail.com>
2024-06-21 08:53:45 -0700

    Added: Initial checkin to the Database page

---
+++ MLDas.Web/Pages/Database.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MLDas.Web.Pages
+{
+    public class DatabaseModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
