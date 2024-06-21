dae4ef6ed6e419dc4e4473ab8de7fac4b3969d2f
Jarred Capellman <jcapellman@hotmail.com>
2024-06-20 22:08:28 -0700

    Added: Initial Commit

---
+++ MLDas.Web/Program.cs
@@ -0,0 +1,32 @@
+namespace MLDas.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
