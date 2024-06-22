34548b9402631ea40e1003d82ee02bb54b51a22d
Jarred Capellman <jcapellman@hotmail.com>
2024-06-22 11:18:26 -0700

    Added: Releases Placeholder page

---
+++ MLDas.Web/Pages/Releases.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MLDas.Web.Pages
+{
+    public class ReleasesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
