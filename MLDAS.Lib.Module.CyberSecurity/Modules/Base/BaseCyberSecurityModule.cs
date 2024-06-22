0e26b53c6eaa474bc46a3abcadb9ef61d2ba1da5
Jarred Capellman <jcapellman@hotmail.com>
2024-06-22 14:05:30 -0700

    Added: CyberSecurity Module with a basic implementation

---
+++ MLDAS.Lib.Module.CyberSecurity/Modules/Base/BaseCyberSecurityModule.cs
@@ -0,0 +1,10 @@
+﻿using MLDAS.Lib.Platform.Abstractions.Base;
+using MLDAS.Lib.Platform.Enums;
+
+namespace MLDAS.Lib.Category.CyberSecurity.Modules.Base
+{
+    public abstract class BaseCyberSecurityModule : BaseModule
+    {
+        public override Categories Category => Categories.CyberSecurity;
+    }
+}
---
+++ MLDAS.Lib.Module.CyberSecurity/Modules/FileClassificationModule.cs
@@ -0,0 +1,16 @@
+﻿using MLDAS.Lib.Category.CyberSecurity.Modules.Base;
+
+namespace MLDAS.Lib.Category.CyberSecurity.Modules
+{
+    public class FileClassificationModule : BaseCyberSecurityModule
+    {
+        public override string Name => "File Classification";
+
+        public override string Description => "Classifies the PE32 file as malware or not";
+
+        public override void Run()
+        {
+            // TODO
+        }
+    }
+}
