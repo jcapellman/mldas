451bac84afcf944453bcc529989f7257f0236b7b
Jarred Capellman <jcapellman@hotmail.com>
2024-06-22 20:31:47 -0700

    Added: ModuleManager class

---
+++ MLDAS.Lib.Platform/Managers/ModuleManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+
+using MLDAS.Lib.Platform.Abstractions.Base;
+using MLDAS.Lib.Platform.Enums;
+using MLDAS.Lib.Platform.Managers.Base;
+
+namespace MLDAS.Lib.Platform.Managers
+{
+    public class ModuleManager : BaseManager
+    {
+        private readonly List<BaseModule> _availableModules = new List<BaseModule>();
+
+        public ModuleManager()
+        {
+            LoadModules();
+        }
+
+        private void LoadModules()
+        {
+            var potentialModules = Directory.GetFiles(CurrentDirectory, "MLDAS.Lib.Category.*.dll");
+
+            foreach (var potentialModulePath in potentialModules)
+            {
+                var assembly = Assembly.LoadFile(potentialModulePath);
+
+                _availableModules.AddRange(assembly.GetTypes().Where(a => a.BaseType?.BaseType == typeof(BaseModule) && !a.IsAbstract).Select(b => (BaseModule)Activator.CreateInstance(b)!));
+            }
+        }
+
+        public IEnumerable<BaseModule> GetModulesByCategory(Categories category) => _availableModules.Where(a => a.Category == category);
+    }
+}
