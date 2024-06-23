c13b864075e04504d4dbe25ad438e2a29075487e
Jarred Capellman <jcapellman@hotmail.com>
2024-06-22 20:32:16 -0700

    Added: Module Loading on the Index page

--- MLDas.Web/Pages/Index.cshtml.cs
+++ MLDas.Web/Pages/Index.cshtml.cs
@@ -1,20 +1,19 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using MLDAS.Lib.Platform.Abstractions.Base;
+using MLDAS.Lib.Platform.Enums;
+using MLDAS.Lib.Platform.Managers;
 
 namespace MLDas.Web.Pages
 {
-    public class IndexModel : PageModel
+    public class IndexModel(ILogger<IndexModel> logger, ModuleManager moduleManager) : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly ILogger<IndexModel> _logger = logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+        public IEnumerable<BaseModule> Modules { get; private set; } = moduleManager.GetModulesByCategory(Categories.CyberSecurity);
 
         public void OnGet()
         {
-
         }
     }
 }
